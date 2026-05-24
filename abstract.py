db273c6cda29e9ee2b3684a0747137e28f2780d1
rfg2110 <rfg2110@gmail.com>
2026-05-24 17:14:44 +0300

    Update abstract.py

--- abstract.py
+++ abstract.py
@@ -1,8 +1,60 @@
-a = 7
-b = -3
+import math 
+a = int(input())
+# b = int(input())
 
-print(-a + b + 7)
-print(a + 2 * b)
-print(-a + b + 3)
-print((a + b) / 4)
-print((a + 2) / b)
+# num = int(input())
+# flag = True
+counter = 0
+
+for i in range(a):
+    num = 1 / (i + 1)
+    counter += num 
+
+ln_x = math.log(a)
+print(counter - ln_x)
+
+# if num == 1:
+#     print('Это единица, она не простая и не составная') 
+# elif flag == True:
+#     print('Число простое')
+# else:
+#     print('Число составное')
+
+print(1 / a)
+# if a <= b:
+#     for i in range(a, b + 1, 1):
+#         print(i)
+# elif a > b:
+#     for i in range(a, b - 1, -1):
+#         print(i)
+
+    
+# for i in range(a, b+1):
+#     print(i )   
+
+# c = float(input())
+# d = float(input())
+
+# if a == 1 or a == 2 : 3384390.4339
+#     if a == 1:
+#         print(a * 10.5)
+#     else :
+#         print(int(2 * 10.5))
+# elif a <= 25:
+#     print(((a - 2) * 4) + 21)
+# else :
+#     print("Пока столько не живут(")
+
+
+
+#условие на диоганаль a != c and b != d
+
+# if b > c:
+#     print("отрезок")
+# elif b == c:
+#     print("точка")
+# elif b < c:
+#     print("пустое множество")
+# else :
+#     print("WTF")
+
